550034615a2f9485bab677daef28dcab17cbf8e3
marianabelchior <97919257+marianabelchior@users.noreply.github.com>
2022-08-13 20:12:18 +0100

    Add files via upload

---
+++ exercicio44/scripts/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.tn_item').colorbox({
+        width:400,
+        height:300
+    });
+        
+    $('.area_login').colorbox({
+        width:400,
+        height:100
+    })
+
+})
